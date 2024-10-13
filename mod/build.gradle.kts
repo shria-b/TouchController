c0d88d4485d0496f9c238ec4fe5bcf49e4df9874
fifth_light <liu-yu-jie@foxmail.com>
2024-10-13 17:52:03 +0800

    Gradle 配置文件全部使用 Kotlin DSL

---
+++ mod/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.fabric.loom)
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+}
+
+version = "1.0.0"
+group = "top.fifthlight.touchcontroller"
+
+var modName = "TouchController"
+base {
+    archivesName = modName
+}
+
+repositories {
+    maven {
+        name = "Terraformers"
+        url = uri("https://maven.terraformersmc.com/")
+    }
+}
+
+dependencies {
+    minecraft(libs.minecraft)
+    mappings("net.fabricmc:yarn:${libs.versions.yarn.get()}:v2")
+    modImplementation(libs.fabric.loader)
+
+    modImplementation(libs.fabric.api)
+    modImplementation(libs.fabric.language.kotlin)
+    modImplementation(libs.modmenu)
+
+    implementation(project(":proxy"))
+    implementation(libs.koin.core)
+    implementation(libs.koin.logger.slf4j)
+    implementation(libs.kotlinx.coroutines.core)
+}
+
+tasks.withType<ProcessResources> {
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand(mapOf(
+            "version" to project.version,
+            "name" to modName,
+        ))
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+
+    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+    // if it is present.
+    // If you remove this line, sources will not be generated.
+    withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<Jar> {
+    from(File(rootDir, "LICENSE")) {
+        rename { "${it}_${modName}"}
+    }
+}
