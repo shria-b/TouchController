f911c65039db87f55a1c55ce7b358607ede9c97f
fifth_light <liu-yu-jie@foxmail.com>
2024-12-12 14:28:21 +0800

    修复攻击指示器设置在十字准心时会跟随十字准心一起隐藏的问题

--- mod/src/main/java/top/fifthlight/touchcontroller/mixin/InGameHudMixin.java
+++ mod/src/main/java/top/fifthlight/touchcontroller/mixin/InGameHudMixin.java
@@ -1,9 +1,16 @@
 package top.fifthlight.touchcontroller.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.option.AttackIndicator;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,10 +18,42 @@ import top.fifthlight.touchcontroller.event.HudRenderCallback;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-    @Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
-    public void renderCrosshair(DrawContext drawContext, RenderTickCounter tickCounter, CallbackInfo callbackInfo) {
-        boolean shouldRender = HudRenderCallback.INSTANCE.getCROSSHAIR().invoker().onCrosshairRender(drawContext, tickCounter);
+    @Shadow @Final private MinecraftClient client;
+
+    @Shadow @Final private static Identifier CROSSHAIR_ATTACK_INDICATOR_FULL_TEXTURE;
+
+    @Shadow @Final private static Identifier CROSSHAIR_ATTACK_INDICATOR_BACKGROUND_TEXTURE;
+
+    @Shadow @Final private static Identifier CROSSHAIR_ATTACK_INDICATOR_PROGRESS_TEXTURE;
+
+    @Inject(
+            method = "renderCrosshair",
+            at = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V",
+                ordinal = 0
+            ),
+            cancellable = true
+    )
+    public void renderCrosshair(DrawContext context, RenderTickCounter tickCounter, CallbackInfo callbackInfo) {
+        boolean shouldRender = HudRenderCallback.INSTANCE.getCROSSHAIR().invoker().onCrosshairRender(context, tickCounter);
         if (!shouldRender) {
+            if (this.client.options.getAttackIndicator().getValue() == AttackIndicator.CROSSHAIR) {
+                float attackCooldownProgress = this.client.player.getAttackCooldownProgress(0.0f);
+                boolean renderFullTexture = false;
+                if (this.client.targetedEntity != null && this.client.targetedEntity instanceof LivingEntity && attackCooldownProgress >= 1.0f) {
+                    renderFullTexture = this.client.player.getAttackCooldownProgressPerTick() > 5.0f && this.client.targetedEntity.isAlive();
+                }
+                int x = context.getScaledWindowWidth() / 2 ;
+                int y = context.getScaledWindowHeight() / 2;
+                if (renderFullTexture) {
+                    context.drawGuiTexture(RenderLayer::getCrosshair, CROSSHAIR_ATTACK_INDICATOR_FULL_TEXTURE, x - 8, y - 8, 16, 16);
+                } else if (attackCooldownProgress < 1.0f) {
+                    int progress = (int)(attackCooldownProgress * 17.0f);
+                    context.drawGuiTexture(RenderLayer::getCrosshair, CROSSHAIR_ATTACK_INDICATOR_BACKGROUND_TEXTURE, x - 8, y - 2, 16, 4);
+                    context.drawGuiTexture(RenderLayer::getCrosshair, CROSSHAIR_ATTACK_INDICATOR_PROGRESS_TEXTURE, 16, 4, 0, 0, x - 8, y - 2, progress, 4);
+                }
+            }
             callbackInfo.cancel();
         }
     }
