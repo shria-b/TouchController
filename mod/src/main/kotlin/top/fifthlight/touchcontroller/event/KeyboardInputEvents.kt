78da9709dfcb56fba4b7d428a538ef25b2065b2a
shria-b <159120355+shria-b@users.noreply.github.com>
2024-12-13 23:27:32 +0800

    Update KeyboardInputEvents.kt

--- mod/src/main/kotlin/top/fifthlight/touchcontroller/event/KeyboardInputEvents.kt
+++ mod/src/main/kotlin/top/fifthlight/touchcontroller/event/KeyboardInputEvents.kt
@@ -3,7 +3,6 @@ package top.fifthlight.touchcontroller.event
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory
 import net.minecraft.client.input.KeyboardInput
-import top.fifthlight.touchcontroller.event.KeyboardInputEvents.EndInputTick
 
 object KeyboardInputEvents {
     val END_INPUT_TICK: Event<EndInputTick> = EventFactory.createArrayBacked(
