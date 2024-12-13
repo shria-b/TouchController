84198107e95b3cabbe3d5f549b0aebaa9dca462f
shria-b <159120355+shria-b@users.noreply.github.com>
2024-12-13 23:34:07 +0800

    Update KeyboardInputEvents.kt

--- mod/src/main/kotlin/top/fifthlight/touchcontroller/event/KeyboardInputEvents.kt
+++ mod/src/main/kotlin/top/fifthlight/touchcontroller/event/KeyboardInputEvents.kt
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.event.EventFactory
 import net.minecraft.client.input.KeyboardInput
 
 object KeyboardInputEvents {
-    // 确保将END_INPUT_TICK声明为public
+    // 将END_INPUT_TICK声明为public以允许在其他包中访问
     public val END_INPUT_TICK: Event<EndInputTick> = EventFactory.createArrayBacked(
         EndInputTick::class.java
     ) { callbacks: Array<EndInputTick> ->
